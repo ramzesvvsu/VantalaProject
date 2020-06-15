3c29c0f6a67aac7e679663261e7224b7172a08fc
Гаманец Роман <ramzesvvsu@gmail.com>
2020-06-16 00:50:26 +0300

    Исправил алгоритм расчета

--- CommonStatistic.py
+++ CommonStatistic.py
@@ -11,18 +11,22 @@ import pprint as pp
 
 
 def print_statistic(file, div_list):
-    df = pd.read_csv(file, names=['Name', 'per', 'Date', 'time', 'Price']).iloc[1:]
-    df.drop(['Name', 'time', 'per'], axis=1, inplace=True)
-    df.Date = pd.to_datetime(df.Date)
-    df.Price = pd.to_numeric(df.Price)
-    div_list.Date = pd.to_datetime(div_list.Date)
+    df = pd.read_csv(file, names=['DATE', 'TIME', 'OPEN', 'HIGH', 'LOW', 'CLOSE', 'VOL'])
+    df.drop(['LOW', 'TIME', 'HIGH', 'VOL'], axis=1, inplace=True)
+    df.DATE = pd.to_datetime(df.DATE, dayfirst=True)
+    df.OPEN = pd.to_numeric(df.OPEN)
+    df.CLOSE = pd.to_numeric(df.CLOSE)
+    df['Price'] = (df.OPEN + df.CLOSE)/2
+    df['Price2'] = df.Price.round(2)
+    df.Price = df.Price.round(2)
+    div_list.Date = pd.to_datetime(div_list.Date) - pd.offsets.MonthBegin(1)
     div_list.sort_values(by='Date', inplace=True, ascending=False)
     df['Qty'] = 0
     df['QtyNoDiv'] = 0
     df['Amount'] = 0
     df['AmountNoDiv'] = 0
     df['Cash'] = 0
-    df.loc[1, 'Cash'] = 1000000
+    df.loc[0, 'Cash'] = 100000
     last_div = None
     prev_row = None
     sum_div = None
@@ -33,22 +37,20 @@ def print_statistic(file, div_list):
         if row.Cash == 0:
             row.Cash = prev_row.Cash
             row.Qty = prev_row.Qty
-        found_div = div_list[div_list.Date <= row.Date]
+        found_div = div_list[div_list.Date == row.DATE]
         if len(found_div) > 0:
-            if last_div == None or not last_div == found_div.Date.values[0]:
-                last_div = found_div.Date.values[0]
-                sum_div = row.Qty * found_div.Val.values[0]
-                row.Cash += row.Qty * found_div.Val.values[0]
+            sum_div = row.Qty * found_div.Val.values[0]
+            row.Cash += sum_div
 
-        if row.Cash // row.Price > 0:
+        if row.Cash // row.Price > 0 and (sum_div == None or sum_div > 0):
             new_qty = row.Cash // row.Price
             if sum_div == None:
-                result_list.append([row.Date.to_datetime64(), new_qty, 0, 0, row.Price, new_qty,
-                                    row.Cash - new_qty * row.Price])
+                result_list.append([row.DATE.to_datetime64(), new_qty, 0, 0, row.Price, new_qty,
+                                    row.Cash - new_qty * row.Price, row.Cash])
             else:
                 result_list.append(
-                    [row.Date.to_datetime64(), row.Qty, sum_div, found_div.Val.values[0] if sum_div > 0 else 0,
-                     row.Price, new_qty, row.Cash - new_qty * row.Price])
+                    [row.DATE.to_datetime64(), row.Qty, sum_div, found_div.Val.values[0] if sum_div > 0 else 0,
+                     row.Price, new_qty, row.Cash - new_qty * row.Price, row.Qty*row.Price+row.Cash])
                 sum_div = 0
             row.Qty += new_qty
             row.Cash = row.Cash - new_qty * row.Price
@@ -61,18 +63,19 @@ def print_statistic(file, div_list):
         df.loc[index, 'Cash'] = row.Cash
         prev_row = row
     result_list.append(
-        [prev_row.Date.to_datetime64(), prev_row.Qty, 0, 0, prev_row.Price, 0, prev_row.Cash])
+        [prev_row.DATE.to_datetime64(), prev_row.Qty, 0, 0, prev_row.Price, 0, prev_row.Cash,
+         prev_row.Cash + prev_row.Qty*prev_row.Price])
 
     result_table = pd.DataFrame(result_list,
                                 columns=['Дата', 'Количество акций', 'Общая сумма дивов', 'Дивов на 1 акцию',
                                          'Цена акции',
-                                         'Количество акций новых', 'Остаток денег'])
+                                         'Количество акций новых', 'Остаток денег', 'Стоимость актива'])
     pd.set_option('display.max_columns', 20)
     pd.set_option('display.width', 200)
     display(result_table)
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=df.Date, y=df.AmountNoDiv, name='Сумма без дивидентов'))
-    fig.add_trace(go.Scatter(x=df.Date, y=df.Amount, name='Сумма'))
+    fig.add_trace(go.Scatter(x=df.DATE, y=df.AmountNoDiv, name='Сумма без дивидентов'))
+    fig.add_trace(go.Scatter(x=df.DATE, y=df.Amount, name='Сумма'))
     fig.update_layout(
         legend_orientation="h",
         title="График роста стоимости портфеля",
@@ -86,7 +89,7 @@ def print_statistic(file, div_list):
     )
     fig.show()
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=df.Date, y=df.Qty, name='Количество акций'))
+    fig.add_trace(go.Scatter(x=df.DATE, y=df.Qty, name='Количество акций'))
     fig.update_layout(
         legend_orientation="h",
         title="График роста количества акций",
@@ -100,40 +103,23 @@ def print_statistic(file, div_list):
     fig.show()
 
 if __name__ == '__main__':
-    print_statistic('LKOH_100101_200101.txt', pd.DataFrame.from_dict({'Date': ['09.06.2020',
-                                                                               '20.12.2019',
-                                                                               '09.06.2019',
-                                                                               '21.12.2018',
-                                                                               '11.06.2018',
-                                                                               '22.12.2017',
-                                                                               '10.06.2017',
-                                                                               '23.12.2016',
-                                                                               '12.06.2016',
-                                                                               '24.12.2015',
-                                                                               '14.06.2015',
-                                                                               '26.12.2014',
-                                                                               '15.06.2014',
-                                                                               '15.08.2013',
-                                                                               '13.05.2013',
-                                                                               '12.11.2012',
-                                                                               '11.05.2012',
-                                                                               '6.05.2011',
-                                                                               '7.05.2010'], 'Val': [170.00,
-                                                                                                     192.00,
-                                                                                                     155.00,
-                                                                                                     95.00,
-                                                                                                     130.00,
-                                                                                                     85.00,
-                                                                                                     120.00,
-                                                                                                     75.00,
-                                                                                                     112.00,
-                                                                                                     65.00,
-                                                                                                     94.00,
-                                                                                                     60.00,
-                                                                                                     60.00,
-                                                                                                     50.00,
-                                                                                                     50.00,
-                                                                                                     40.00,
-                                                                                                     75.00,
-                                                                                                     59.00,
-                                                                                                     52.00]}))
+    print_statistic('SBER_100101_200101.csv', pd.DataFrame.from_dict({'Date': ['13.06.2019',
+                                                                               '26.06.2018',
+                                                                               '14.06.2017',
+                                                                               '14.06.2016',
+                                                                               '15.06.2015',
+                                                                               '17.06.2014',
+                                                                               '11.04.2013',
+                                                                               '12.04.2012',
+                                                                               '15.04.2011',
+                                                                               '16.04.2010'], 'Val': [16.00,
+                                                                                                      12.00,
+                                                                                                      6.00,
+                                                                                                      1.97,
+                                                                                                      0.45,
+                                                                                                      3.20,
+                                                                                                      3.20,
+                                                                                                      2.59,
+                                                                                                      1.15,
+                                                                                                      0.45
+                                                                                                      ]}))
