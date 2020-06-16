4e607c257ae6c1c7cb1b9c2f892c580eb61950d7
Гаманец Роман <ramzesvvsu@gmail.com>
2020-06-16 11:32:54 +0300

    Передел правила расчета

--- CommonStatistic.py
+++ CommonStatistic.py
@@ -1,25 +1,26 @@
 import plotly
+
 plotly.offline.init_notebook_mode()
 import plotly.graph_objs as go
 import plotly.express as px
 from plotly.subplots import make_subplots
-
+import datetime
 from IPython.display import display
 import numpy as np
 import pandas as pd
 import pprint as pp
 
 
-def print_statistic(file, div_list):
+def print_statistic(file, div_list, start_price, end_price):
     df = pd.read_csv(file, names=['DATE', 'TIME', 'OPEN', 'HIGH', 'LOW', 'CLOSE', 'VOL'])
     df.drop(['LOW', 'TIME', 'HIGH', 'VOL'], axis=1, inplace=True)
     df.DATE = pd.to_datetime(df.DATE, dayfirst=True)
     df.OPEN = pd.to_numeric(df.OPEN)
     df.CLOSE = pd.to_numeric(df.CLOSE)
-    df['Price'] = (df.OPEN + df.CLOSE)/2
+    df['Price'] = (df.OPEN + df.CLOSE) / 2
     df['Price2'] = df.Price.round(2)
     df.Price = df.Price.round(2)
-    div_list.Date = pd.to_datetime(div_list.Date) - pd.offsets.MonthBegin(1)
+    div_list.Date = pd.to_datetime(div_list.Date)
     div_list.sort_values(by='Date', inplace=True, ascending=False)
     df['Qty'] = 0
     df['QtyNoDiv'] = 0
@@ -27,6 +28,7 @@ def print_statistic(file, div_list):
     df['AmountNoDiv'] = 0
     df['Cash'] = 0
     df.loc[0, 'Cash'] = 100000
+    df.loc[0, 'Price'] = start_price
     last_div = None
     prev_row = None
     sum_div = None
@@ -37,10 +39,11 @@ def print_statistic(file, div_list):
         if row.Cash == 0:
             row.Cash = prev_row.Cash
             row.Qty = prev_row.Qty
-        found_div = div_list[div_list.Date == row.DATE]
-        if len(found_div) > 0:
+        found_div = div_list[div_list.Date <= row.DATE - datetime.timedelta(days=1)]
+        if len(found_div) > 0 and not found_div.Date.values[0] == last_div:
             sum_div = row.Qty * found_div.Val.values[0]
             row.Cash += sum_div
+            last_div = found_div.Date.values[0]
 
         if row.Cash // row.Price > 0 and (sum_div == None or sum_div > 0):
             new_qty = row.Cash // row.Price
@@ -50,7 +53,7 @@ def print_statistic(file, div_list):
             else:
                 result_list.append(
                     [row.DATE.to_datetime64(), row.Qty, sum_div, found_div.Val.values[0] if sum_div > 0 else 0,
-                     row.Price, new_qty, row.Cash - new_qty * row.Price, row.Qty*row.Price+row.Cash])
+                     row.Price, new_qty, row.Cash - new_qty * row.Price, row.Qty * row.Price + row.Cash])
                 sum_div = 0
             row.Qty += new_qty
             row.Cash = row.Cash - new_qty * row.Price
@@ -63,8 +66,8 @@ def print_statistic(file, div_list):
         df.loc[index, 'Cash'] = row.Cash
         prev_row = row
     result_list.append(
-        [prev_row.DATE.to_datetime64(), prev_row.Qty, 0, 0, prev_row.Price, 0, prev_row.Cash,
-         prev_row.Cash + prev_row.Qty*prev_row.Price])
+        [prev_row.DATE.to_datetime64(), prev_row.Qty, 0, 0, end_price, 0, prev_row.Cash,
+         prev_row.Cash + prev_row.Qty * end_price])
 
     result_table = pd.DataFrame(result_list,
                                 columns=['Дата', 'Количество акций', 'Общая сумма дивов', 'Дивов на 1 акцию',
@@ -102,24 +105,28 @@ def print_statistic(file, div_list):
         ))
     fig.show()
 
+
 if __name__ == '__main__':
-    print_statistic('SBER_100101_200101.csv', pd.DataFrame.from_dict({'Date': ['13.06.2019',
-                                                                               '26.06.2018',
-                                                                               '14.06.2017',
-                                                                               '14.06.2016',
-                                                                               '15.06.2015',
-                                                                               '17.06.2014',
-                                                                               '11.04.2013',
-                                                                               '12.04.2012',
-                                                                               '15.04.2011',
-                                                                               '16.04.2010'], 'Val': [16.00,
-                                                                                                      12.00,
-                                                                                                      6.00,
-                                                                                                      1.97,
-                                                                                                      0.45,
-                                                                                                      3.20,
-                                                                                                      3.20,
-                                                                                                      2.59,
-                                                                                                      1.15,
-                                                                                                      0.45
-                                                                                                      ]}))
+    start_price = round((86.56 + 88.41) / 2, 2)
+    end_price = round((255.99 + 252.20) / 2, 2)
+    print_statistic('SBER_100101_200101.csv',
+                    pd.DataFrame.from_dict({'Date': ['13.06.2019',
+                                                     '26.06.2018',
+                                                     '14.06.2017',
+                                                     '14.06.2016',
+                                                     '15.06.2015',
+                                                     '17.06.2014',
+                                                     '11.04.2013',
+                                                     '12.04.2012',
+                                                     '15.04.2011',
+                                                     '16.04.2010'], 'Val': [16.00,
+                                                                            12.00,
+                                                                            6.00,
+                                                                            1.97,
+                                                                            0.45,
+                                                                            3.20,
+                                                                            3.20,
+                                                                            2.59,
+                                                                            1.15,
+                                                                            0.45
+                                                                            ]}), start_price, end_price)
